5902bf2276118775ff8a133a2125b87e0917aa7b
PRADEEPSINGH <pradeepsi2120032@gmail.com>
2023-04-25 20:02:20 +0530

    added creator name to podcasts

--- client/pages/api/apiroutes.js
+++ client/pages/api/apiroutes.js
@@ -1,4 +1,4 @@
-export const host="https://podscape3.onrender.com";
+export const host="http://localhost:5000";
 
 export const podcastroute=`${host}/api/podcasts/createpodcast`;
 export const getallpodcasts=`${host}/api/podcasts/getallpodcasts`;
